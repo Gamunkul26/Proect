b76add9b281674a235ef8e24cf00a172f7209a4f
Gamunkul26 <gleb@menshenin.com>
2023-06-03 18:43:05 +0500

    add todo app

---
+++ src/routes/blog/posts/2/+page.svelte
@@ -0,0 +1,46 @@
+<h1>My Second Blog Post</h1>
+<p class="lorem200">
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit magni modi
+    omnis sit officiis facilis amet tempore. Quod dolorem sapiente sit dolor in
+    perferendis quaerat est vero recusandae sunt. Odio veniam eveniet, fugit
+    dolor dolores nihil ab! Aliquid sunt laudantium ipsum rerum recusandae
+    facere. Quae natus quo unde qui iusto cumque facere culpa nulla
+    necessitatibus excepturi vero dolore magnam, odio voluptatem doloribus
+    quibusdam ipsum id expedita explicabo hic minima adipisci recusandae. Alias
+    architecto veniam exercitationem obcaecati in similique a temporibus!
+    Minima, sequi nam culpa cupiditate nisi, illum non esse necessitatibus quod
+    itaque, tempore aspernatur corrupti quos veritatis! Perspiciatis nostrum
+    maiores velit nulla tempora. Quod mollitia perferendis ullam corporis
+    corrupti laudantium debitis sit, omnis, vero, voluptatem voluptas
+    architecto. Ad nesciunt ratione aliquid exercitationem, quidem porro. Facere
+    suscipit eaque at obcaecati! Voluptatem, eos commodi corrupti earum ipsum
+    ducimus delectus, eaque vitae odio nobis sequi voluptate rem architecto
+    atque doloribus magni, suscipit autem sint nostrum! Qui deleniti doloremque
+    voluptates vitae impedit incidunt asperiores laudantium accusantium minima
+    facilis tempore illo inventore beatae alias laboriosam repudiandae labore,
+    unde modi quae? Modi officia voluptas eius hic quam ducimus iure quia,
+    similique eveniet repellendus impedit, dolorum reiciendis doloremque
+    perspiciatis numquam molestiae repellat eos totam voluptatem, quidem libero.
+</p>
+<p class="lorem200">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quae pariatur
+    quisquam ducimus iusto aspernatur eaque repudiandae. Tempore accusantium,
+    officia ex placeat libero aliquam temporibus nesciunt quis eum earum fugit
+    similique natus impedit. Veritatis minus, eaque quod animi odit quos
+    suscipit in soluta fuga. Velit officia, praesentium ipsum mollitia libero
+    assumenda quis eius nesciunt quos iste adipisci, autem aut sint vitae veniam
+    recusandae consectetur tempora, expedita similique! Ullam maxime, expedita
+    quibusdam nobis, ea, laboriosam sunt magni omnis laborum necessitatibus
+    exercitationem! Suscipit eligendi magnam minus illo eius facilis iusto
+    itaque, quis quas dolores nihil culpa officiis corporis minima esse unde
+    impedit tempora cumque necessitatibus commodi porro quae maiores odio eaque.
+    Deleniti beatae ipsa explicabo voluptatibus maxime dolore fugit in, quod
+    doloribus eius facilis fugiat natus officia aspernatur minus corrupti
+    voluptates eos recusandae nam culpa error nobis amet eligendi vel? A eos quo
+    vero iure obcaecati aut, odio distinctio officia consectetur delectus vel
+    illo! Ea vitae ab ipsam beatae sequi, molestias iusto laborum at labore
+    esse, sit a mollitia laudantium quia accusantium ipsa reprehenderit. Unde
+    placeat recusandae quae molestias natus inventore perspiciatis amet porro.
+    Deleniti ut, ratione quaerat placeat culpa fugiat cumque nobis et voluptate?
+    Nihil, necessitatibus quod. Aliquid, repellendus cupiditate? Nobis.
+</p>
